2a7cd9100b9e8dc7d613e7c1e9e231cb136eb3bd
Pedro Medina <131189713+zyken01@users.noreply.github.com>
2024-10-08 20:19:13 -0600

    Feat:days

---
+++ Facturacion/UNAM/10Octubre/complemento_octubre.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * DATOS PARA GENERAR EL COMPLEMENTO DE PAGO 
+*/
+
+
+// =====================================
+// Cliente Frecuente*:
+$( "#135textboxautocomplete55").val("UNA2907227Y5");
+// Código Postal
+$( "#135textbox61").val("04510");
+// Régimen Fiscal*:
+$( "#135textboxautocomplete62").val("Personas Morales con Fines no Lucrativos");
+
+
+// Total pago
+$( "#156textbox24").val("11075");
+
+// RFC
+$( "#156textbox36").val("BBA830831LJ2");
+// NOMBRE BANCO
+$( "#156textbox37").val("BBVABANCOMERSA");
+// CUENTA ORDENANTE
+$( "#156textbox38").val("0445781337");
+
+// RFC EMISOR CUENTA BENEFICIARIA
+$( "#156textbox41").val("BSM970519DU8");
+
+// CUENTA BENEFICIARIO
+$( "#156textbox42").val("6744355165");
